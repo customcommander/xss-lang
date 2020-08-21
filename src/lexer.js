6d6c690de5c0a857c88af988de7adec6bf2ef987
customcommander <hello@spinjs.com>
2020-08-21 06:46:37 +0000

    refactor: lexer: split js_proto regex into smaller ones

--- src/lexer.js
+++ src/lexer.js
@@ -2,10 +2,26 @@
 
 const moo = require('moo');
 
+/*
+  Building individual regexes as strings to make it easier to the eye.
+  We then build the final regex by joining them altogether.
+  Note: moo doesnt' support the "i" flag for regex!
+*/
+const j = '(?:j|J)';
+const a = '(?:a|A)';
+const v = '(?:v|V)';
+const s = '(?:s|S)';
+const c = '(?:c|C)';
+const r = '(?:r|R)';
+const i = '(?:i|I)';
+const p = '(?:p|P)';
+const t = '(?:t|T)';
+const proto_end = ':';
+
 module.exports =
   moo.compile
   ( { js_protocol:
-        { match: /[jJ][aA][vV][aA][sS][cC][rR][iI][pP][tT]:/
+        { match: new RegExp(j+a+v+a+s+c+r+i+p+t+proto_end)
         , value: () => 'javascript:'
         }
     , any: /.+/
